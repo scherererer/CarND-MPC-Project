5227d0d8552f5433c5a8efadde6c91790a8d2f55
Michael P. Scherer <scherer@protonmail.ch>
2017-08-13 20:54:06 -0500

    Reordered headers, tyle cleanups, reflowed comments

--- src/MPC.h
+++ src/MPC.h
@@ -1,20 +1,19 @@
-#ifndef MPC_H
-#define MPC_H
+#pragma once
+
+#include "Eigen-3.3/Eigen/Core"
 
 #include <vector>
-#include "Eigen-3.3/Eigen/Core"
 
 using namespace std;
 
-class MPC {
- public:
-  MPC();
+class MPC
+{
+public:
+	MPC();
 
-  virtual ~MPC();
+	virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+	// Solve the model given an initial state and polynomial coefficients.
+	// Return the first actuatotions.
+	vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
-
-#endif /* MPC_H */
